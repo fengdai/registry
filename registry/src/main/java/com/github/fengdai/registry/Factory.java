a06af5830706bfc6d785e1c42d03064b11461fdb
fengdai <toxic.dai@gmail.com>
2016-04-07 11:33:45 +0800

    Fixed Retention & target of annotations.

--- registry/src/main/java/com/github/fengdai/registry/Factory.java
+++ registry/src/main/java/com/github/fengdai/registry/Factory.java
@@ -1,5 +1,12 @@
 package com.github.fengdai.registry;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE })
 public @interface Factory {
 
   Class<? extends ViewFactory<?>> value();
--- registry/src/main/java/com/github/fengdai/registry/Layout.java
+++ registry/src/main/java/com/github/fengdai/registry/Layout.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
-@Target({ ElementType.TYPE, ElementType.FIELD })
+@Target({ ElementType.TYPE })
 public @interface Layout {
 
   int value();
